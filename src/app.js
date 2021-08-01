f8ef0e2276d620bcc744efd046a6af5b17cd531b
omorest <oscar.moreira.estevez@gmail.com>
2021-08-01 23:42:58 +0100

    delete comments

--- src/app.js
+++ src/app.js
@@ -14,15 +14,13 @@ app.listen(process.env.PORT || 5000, () => {
   console.log('Server running on localhost:5000...')
 })
 
-// app.get('/api/location/:city/:country?', cors(corsOptions), async (request, response) => {
-app.get('/api/location/:city/:country?', async (request, response) => {
+app.get('/api/location/:city/:country?', cors(corsOptions), async (request, response) => {
   const {city, country} = request.params;
   const data = await getDataWeatherByCity(city, country)
   response.json(data);
 })
 
-// app.get('/api/coords/:latitude/:longitude', cors(corsOptions), async (request, response) => {
-app.get('/api/coords/:latitude/:longitude', async (request, response) => {
+app.get('/api/coords/:latitude/:longitude', cors(corsOptions), async (request, response) => {
   const {latitude, longitude} = request.params;
   const data = await getDataWeatherByLatLon(latitude, longitude);
   response.json(data);
